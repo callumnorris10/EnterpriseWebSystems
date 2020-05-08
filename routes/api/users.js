ea15e82e33e3c9ff54eb56b467291a9af16273a6
Callum <callumnorris10@googlemail.com>
2020-05-08 17:27:17 +0100

    addition of jwt authentication tokens for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User.js");
@@ -53,13 +55,25 @@ router.post(
       await user.save();
 
       //return json web token
-      res.send("user successfully registered");
+      const payload = {
+        user: { id: user.id },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
     }
 
-    res.send("this is the user route");
+    //res.send("this is the user route");
   }
 );
 
