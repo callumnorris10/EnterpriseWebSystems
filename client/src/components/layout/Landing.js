f7301c8d51719b81043b1903b458187da84fc63e
Callum <callumnorris10@googlemail.com>
2020-05-09 18:21:49 +0100

    react frontend setup and navbar and splash screen created

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,25 @@
+import React from "react";
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">CoachLink</h1>
+          <p className="lead">
+            Create a coaching profile, share your experiences and interact with
+            other sports coaches.
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Landing;
