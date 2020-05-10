d59dde1f63f4e82a3c4fda976f70575866d8f490
Callum <callumnorris10@googlemail.com>
2020-05-10 20:21:57 +0100

    no progress

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const cors = require("cors");
 
 //connect the database
 
@@ -15,7 +16,7 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
-
+app.use(cors({ origin: "http://localhost:3000" }));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server successfully on port ${PORT}`));
